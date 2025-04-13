962c8320209ea759507652d3c8055530dfe0b445
Will Campbell <62625260+wibb1@users.noreply.github.com>
2025-04-13 12:29:58 -0400

    Simplify SecretKeyEncoder.java

--- src/main/java/com/springboot3security/util/SecretKeyEncoder.java
+++ src/main/java/com/springboot3security/util/SecretKeyEncoder.java
@@ -1,31 +1,18 @@
 package com.springboot3security.util;
-import org.springframework.beans.factory.annotation.Value;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.nio.file.Paths;
 import java.util.Base64;
-import java.util.Properties;
 
 public class SecretKeyEncoder {
-    public static void main(String[] args) {
-        try {
-            // Load the application.properties file
-            Properties properties = new Properties();
-            String propertiesPath = Paths.get("src/main/resources/application.properties").toAbsolutePath().toString();
-            properties.load(new FileInputStream(propertiesPath));
 
-            // Get the jwt.secret value
-            String secret = properties.getProperty("jwt.secret");
-            if (secret == null) {
-                throw new IllegalArgumentException("jwt.secret not found in application.properties");
-            }
-            // Encode the secret key in Base64
-            String base64EncodedKey = Base64.getEncoder().encodeToString(secret.getBytes());
-            System.out.println("Base64 Encoded Key:");
-            System.out.println(base64EncodedKey);
-        } catch (IOException e) {
-            e.printStackTrace();
+    private static final String secret = "gdkj3lv9mz7pn4xqsu5waf82hrtvy0cj6bkoqx1niead3mpzvluw9ytfcork8xhs";
+
+    public static void main(String[] args) {
+        if (secret == null) {
+            throw new IllegalArgumentException("secret cannot be null");
         }
+        // Encode the secret key in Base64
+        String base64EncodedKey = Base64.getEncoder().encodeToString(secret.getBytes());
+        System.out.println("Base64 Encoded Key:");
+        System.out.println(base64EncodedKey);
     }
 }
