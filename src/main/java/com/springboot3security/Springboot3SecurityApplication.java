930c7824ef839b57c4d622416defd98f239aeca9
Will Campbell <62625260+wibb1@users.noreply.github.com>
2025-04-13 16:47:28 -0400

    improving logging

--- src/main/java/com/springboot3security/Springboot3SecurityApplication.java
+++ src/main/java/com/springboot3security/Springboot3SecurityApplication.java
@@ -1,13 +1,16 @@
 package com.springboot3security;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Springboot3SecurityApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(Springboot3SecurityApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot3SecurityApplication.class, args);
+		logger.info("SpringBoot3Security started successfully.");
 	}
 
 }
--- src/main/java/com/springboot3security/filter/JwtAuthFilter.java
+++ src/main/java/com/springboot3security/filter/JwtAuthFilter.java
@@ -5,6 +5,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -19,7 +20,7 @@ import java.io.IOException;
 public class JwtAuthFilter extends OncePerRequestFilter {
 
     private final UserDetailsService userDetailsService;
-    private JwtUtil jwtUtil;
+    private final JwtUtil jwtUtil;
 
     public JwtAuthFilter(UserDetailsService userDetailsService, JwtUtil jwtUtil) {
         this.userDetailsService = userDetailsService;
@@ -27,7 +28,8 @@ public class JwtAuthFilter extends OncePerRequestFilter {
     }
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(HttpServletRequest request, @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException {
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
@@ -35,6 +37,7 @@ public class JwtAuthFilter extends OncePerRequestFilter {
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             token = authHeader.substring(7);
             username = jwtUtil.extractUsername(token);
+            logger.info("Extracted username: " + username);
         }
 
         if (username != null && SecurityContextHolder.getContext().getAuthentication() == null) {
