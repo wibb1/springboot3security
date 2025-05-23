ba7a93b6bf4165b1c16098589ed1a49a0e80d3a9
Will Campbell <62625260+wibb1@users.noreply.github.com>
2025-05-23 18:18:33 -0400

    Update `UserInfoService` to ensure proper dependency injection: - Add constructor injection for `UserDetailsService`, `JwtUtil`, and `PasswordEncoder`. - Ensure `@Service` annotation is correctly applied.

--- src/main/java/com/springboot3security/service/UserInfoService.java
+++ src/main/java/com/springboot3security/service/UserInfoService.java
@@ -8,7 +8,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -65,6 +64,4 @@ public class UserInfoService {
             return "User not found";
         }
     }
-
-
 }
